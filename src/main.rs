f683fd61a116e75d4319da2ac59ee3651188ddac
Oli Dacombe <oliver.dacombe@gsr.io>
2024-02-01 22:27:27 +0000

    implement basic converter from cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use eyre::Result;
 use std::{collections::HashSet, path::PathBuf};
 use terrabastard::{
-    cli::{self, Command},
+    cli::{self, Command, PathArg, Run},
     terraform::{self},
     walk::{self, string_repetitions},
 };
@@ -18,15 +18,16 @@ fn main() -> Result<()> {
     let args = cli::parse()?;
 
     match args.command {
-        Command::Roots => {
-            let roots: HashSet<PathBuf> = walk::find_roots(&args.path).collect();
+        Command::Aws(cmd) => cmd.run(),
+        Command::Roots(PathArg { path }) => {
+            let roots: HashSet<PathBuf> = walk::find_roots(&path).collect();
             println!(
                 "{}",
                 serde_json::to_string_pretty(&roots).unwrap_or("[]".to_string())
             );
         }
-        Command::Parse => {
-            let files: HashSet<PathBuf> = walk::find_files(&args.path).collect();
+        Command::Parse(PathArg { path }) => {
+            let files: HashSet<PathBuf> = walk::find_files(&path).collect();
             for file in files {
                 debug!("Parsing {:?}", &file);
                 let body: Result<hcl::Body> = terraform::parse(&file);
@@ -35,10 +36,10 @@ fn main() -> Result<()> {
                 }
             }
         }
-        Command::Plague => {
+        Command::Plague(PathArg { path }) => {
             println!(
                 "{}",
-                serde_json::to_string_pretty(&string_repetitions(&args.path, 2))
+                serde_json::to_string_pretty(&string_repetitions(&path, 2))
                     .unwrap_or("{}".to_string())
             );
         }
--- src/policy/mod.rs
+++ src/policy/mod.rs
@@ -1,2 +1,16 @@
-pub mod hcl;
+use std::io::Read;
+
+use hcl::Block;
+
+use self::json::PolicyDocument;
+
 pub mod json;
+
+pub fn json_iam_policy_to_data_resource<R: Read, S: AsRef<str>>(
+    json: R,
+    name: S,
+) -> Result<Block, std::io::Error> {
+    let json_policy: PolicyDocument = serde_json::from_reader(json)?;
+    let hcl_policy = json_policy.to_hcl(name.as_ref());
+    Ok(hcl_policy)
+}
